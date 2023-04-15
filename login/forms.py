0fa694519593f064ad53fc55b6c451df10aa118c
Tushar Thonupunoori <tushar29t@gmail.com>
2023-04-15 13:13:24 -0700

    4/15 update

--- login/forms.py
+++ login/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField, IntegerField, PasswordField, DecimalField, \
-                    DecimalRangeField, SelectField, RadioField, IntegerRangeField
+    DecimalRangeField, SelectField, RadioField, IntegerRangeField
 from wtforms.validators import DataRequired, EqualTo, Email, NumberRange, InputRequired
+
 
 class LoginForm(FlaskForm):
     submit_field = SubmitField("Continue")
--- login/start.py
+++ login/start.py
@@ -2,11 +2,13 @@ from socket import SocketIO
 import numpy as np
 import info
 from flask import render_template
+from forms.py import LoginForm
 
 
 def login_start():
-        form = info.LoginForm()
-        return render_template("./login.html", template_title="login", LForm = form)
-    
-if __name__ == '__main__': 
+    form = LoginForm()
+    return render_template("./login.html", template_title="login", LForm=form)
+
+
+if __name__ == '__main__':
     login_start()
