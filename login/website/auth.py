13e68a1acc82ab774d86df769e243565c87cab1c
Rishi Ananth <rishi.k.ananth@gmail.com>
2023-04-23 12:20:29 -0700

    Sign-up Page and Logistics

--- login/website/auth.py
+++ login/website/auth.py
@@ -2,18 +2,34 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
+    if request.method == 'POST':
+         email = request.form.get('email')
+         password = request.form.get('password')
+         user = User.query.filter_by(email=email).first()
+         if user:
+              if check_password_hash(user.password, password):
+                   flash('Logged in successuflly!', category='success')
+                   login_user(user, remember=True)
+                   return redirect(url_for('views.home'))
+              else:
+                   flash('Icorrect password, try again Rishi', category='error')
+         else:
+              flash('Email does not exist.', category='error')
+
+         
     return render_template("login.html")
 
 @auth.route('/logout')
+@login_required
 def logout():
-     return "<p>Logout</p>"
+     logout_user()
+     return redirect(url_for('auth.login'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
@@ -25,7 +41,10 @@ def signup():
           password1 = request.form.get('password1')
           password2 = request.form.get('password2')
 
-          if len(email) < 4:
+          user = User.query.filter_by(email=email).first()
+          if user:
+               flash('Email already exists.', category='error')
+          elif len(email) < 4:
                flash('Email must be greater than 4 characters.', category='error')
           elif len(firstName) < 2:
                flash('First Name must be greater than 1 character.', category='error')
@@ -38,6 +57,8 @@ def signup():
                db.session.add(new_user)
                db.session.commit()
                flash('Account created!', category='success')
+               login_user(user, remember=True)
+
                return redirect(url_for('views.home'))
 
 
